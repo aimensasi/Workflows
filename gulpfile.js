0600d82b3e0d4667f3cdd6e20f12e2907444e21d
AIMEN.S.A.SASI <ali.sasi@ymail.com>
2016-06-09 02:51:24 +0800

    first Compile of tagline.coffee

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp'),
-    gUtil = require('gulp-util');
-
-gulp.task('log', function(){
-    gUtil.log('Work Flow is ');
+    gUtil = require('gulp-util')
+    coffee= require('gulp-coffee');
+var coffeeSources = ['components/coffee/tagline.coffee'];
+gulp.task('coffee', function(){
+    gulp.src(coffeeSources)
+        .pipe(coffee({bare: true}))
+         .on('error', gUtil.log)
+        .pipe(gulp.dest('components/script'))
 });
